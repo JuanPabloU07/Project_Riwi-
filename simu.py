ec6a8f6cf0eea880c26daafadf47b9d68505bf9e
TuNombre <tucorreo@example.com>
2025-11-24 12:13:48 -0500

    jjj

--- simu.py
+++ simu.py
@@ -1,12 +1,11 @@
 inventary = []
 
-
-sales_hitory = []
+sales_history = []
 
 def main_menu():
     while True:
-        print("\n------- Menu principal -------")
-        print("1. Gestionar Inventario")
+        print("\n----- Menu principal -----")
+        print("1. Gestionar inventario")
         print("2. Registrar Venta")
         print("3. Ver Historial de Ventas")
         print("4. Reportes")
@@ -14,13 +13,13 @@ def main_menu():
         option = input("Elige una opción: ")
 
         if option == "1":
-            menu_inventary()
+            inventory_menu()
         elif option == "2":
-            register_vent()
+            register_sale()
         elif option == "3":
             view_sales_history()
         elif option == "4":
-            menu_reports()
+            reports_menu()
         elif option == "5":
             print("¡Hasta luego!")
             break
@@ -28,7 +27,7 @@ def main_menu():
             print("Opción no válida.")
 
 
-def menu_inventary():
+def inventory_menu():
     while True:
         print("\n--- Menu de inventario ---")
         print("1. Registrar libro")
@@ -52,109 +51,109 @@ def menu_inventary():
             print("Opción no válida.")
 
 
-def menu_reports():
+def reports_menu():
     while True:
         print("\n--- Menu de reportes ---")
         print("1. Top 3 libros mas vendidos")
         print("2. Ventas por autor")
         print("3. Reporte de Ingresos")
-        print("4. Rendimiento del Inventario")
-        print("5. Volver")
+        print("4. Volver")
 
         option = input("Elige una opción: ")
 
         if option == "1":
-            top_books_report()
+            report_top_books()
         elif option == "2":
-            sales_report_author()
+            report_sales_author()
         elif option == "3":
-            income_reports()
+            report_income()
         elif option == "4":
-            inventory_performance_report()
-        elif option == "5":
             break
         else:
             print("Opción no válida.")
 
 
-def int_input(msg):
+def input_int(num_int):
     while True:
         try:
-            valor = int_input(msg)
-            return valor
+            value = int(input(num_int))
+            return value
         except ValueError:
             print("Entrada no válida. Ingresa un número entero.")
 
-def input_float(msg):
+def input_float(num_float):
     while True:
         try:
-            valor = float(input(msg))
-            return valor
+            value = float(input(num_float))
+            return value
         except ValueError:
             print("Entrada no válida. Ingresa un número válido.")
 
-def search_book(name):
-    return next((p for p in inventary if p["nombre"].lower() == name.lower()), None)
+def search_book(title):
+    return next((p for p in inventary if p["title"].lower() == title.lower()), None)
 
 
 def register_book():
     print("\n--- Registrar nuevo libro ---")
 
-    name = input("Nombre del libro: ").strip()
-    if search_book(name):
+    title = input("titulo del libro: ").strip()
+    if search_book(title):
         print("El libro ya existe.")
         return
 
     author = input("Autor: ").strip()
     category = input("Categoría: ").strip()
-    price = input_float("Precio unitario: ")
+    price = input_float("precio unitario: ")
     if price <= 0:
         print("El precio debe ser positivo.")
         return
 
-    stock = int(input("Cantidad en stock: "))
+    stock = input_int("Cantidad en stock: ")
     if stock < 0:
         print("El stock no puede ser negativo.")
         return
 
-    garanty = int(input("Garantía (en meses): "))
+    garanty = input_int("Garantía (en meses): ")
 
     book = {
-        "Title": name,
-        "Author": author,
-        "Category": category,
-        "Price": price,
-        "Stock": stock,
-        "Quantity": garanty,
-        "Vendidos": 0
+        "title": title,
+        "author": author,
+        "category": category,
+        "price": price,
+        "stock": stock,
+        "garanty": garanty,
+        "sales": 0
     }
 
     inventary.append(book)
-    print(" Libro registrado exitosamente!")
+    print(" Libro registrado exitosamente")
 
 
 def view_inventary():
-    print("\n--- Listado de libros ---")
+    print("\n--- Listado de inventario ---")
     if not inventary:
         print("El inventario está vacío.")
         return
-    for b in inventary:
-        print(f"{b['title']} | Autor: {b['author']} | Categoría: {b['category']} | " f"Precio: ${b['price']:.2f} | Stock: {b['stock']} | Garantía: {b['quantity']} meses | Vendidos: {b['vendidos']}")
+
+    for i in inventary:
+        print(f"{i['title']} | Autor: {i['author']} | Categoría: {i['category']} | "
+              f"precio: ${i['price']} | Stock: {i['stock']} | Garantía: {i['garanty']} meses")
+
 
 def update_book():
     print("\n--- Actualizar libro ---")
-    name = input("Ingresa el nombre del libro: ")
-    book = search_book(name)
+    title = input("Ingresa el titulo del libro: ")
+    book = search_book(title)
 
     if not book:
-        print("libro no encontrado.")
+        print("Libro no encontrado.")
         return
 
     print("Deja el campo en blanco para mantener el valor actual.")
 
     new_price = input("Nuevo precio: ")
     if new_price.strip():
-        book["precio"] = float(new_price)
+        book["price"] = float(new_price)
 
     new_stock = input("Nuevo stock: ")
     if new_stock.strip():
@@ -162,15 +161,15 @@ def update_book():
 
     new_garanty = input("Nueva garantía (en meses): ")
     if new_garanty.strip():
-        book["garantia"] = int(new_garanty)
+        book["garanty"] = int(new_garanty)
 
     print(" Libro actualizado!")
 
 
 def delete_book():
-    print("\n--- Eliminar libro ---")
-    name = input("Nombre del libro: ")
-    book = search_book(name)
+    print("\n--- Eliminar Libro ---")
+    title = input("Titulo del Libro: ")
+    book = search_book(title)
 
     if not book:
         print("Libro no encontrado.")
@@ -180,18 +179,18 @@ def delete_book():
     print(" Libro eliminado.")
 
 
-def register_vent():
+def register_sale():
     print("\n--- Registrar venta ---")
     customer = input("Nombre del cliente: ")
 
-    name_book = input("Libro vendido: ")
-    book = search_book(name_book)
+    title_book = input("Libro vendido: ")
+    book = search_book(title_book)
 
     if not book:
         print("Libro no encontrado.")
         return
 
-    quantity = int(input("Cantidad vendida: "))
+    quantity = input_int("Cantidad vendida: ")
 
     if quantity > book["stock"]:
         print(f"No hay suficiente stock. Disponible: {book['stock']}")
@@ -200,71 +199,62 @@ def register_vent():
     discount = input_float("Descuento aplicado (%): ")
 
     book["stock"] -= quantity
-    book["vendidos"] += quantity
+    book["sales"] += quantity
 
-    total_price = quantity * book["precio"]
-    final_price = total_price - (total_price * discount / 100)
+    price_total = quantity * book["price"]
+    price_final = price_total - (price_total * discount / 100)
 
-    sales = {
-        "cliente": customer,
-        "libro": book["nombre"],
-        "cantidad": quantity,
-        "descuento": discount,
-        "bruto": total_price,
-        "neto": final_price
+    sale = {
+        "customer": customer,
+        "book": book["title"],
+        "quantity": quantity,
+        "discount": discount,
+        "bruto": price_total,
+        "neto": price_final
     }
 
-    sales_hitory.append(sales)
+    sales_history.append(sale)
     print(" Venta registrada exitosamente!")
 
 
 def view_sales_history():
     print("\n--- Historial de ventas ---")
-    if not sales_hitory:
+    if not sales_history:
         print("No se han registrado ventas.")
         return
 
-    for i in sales_hitory:
-        print(f"{i['cliente']} | {i['producto']} | Cantidad: {i['cantidad']} | " f"Bruto: ${i['bruto']:.2f} | Neto: ${i['neto']:.2f}")
+    for s in sales_history:
+        print(f"{s['customer']} | {s['book']} | quantity: {s['quantity']} | "
+              f"Bruto: ${s['bruto']:.2f} | Neto: ${s['neto']:.2f}")
 
 
 
-def top_books_report():
-    print("\n--- Top 3 libros mas vendidos ---")
-    productos_ordenados = sorted(inventary, key=lambda p: p["vendidos"], reverse=True)
-    for p in productos_ordenados[:3]:
-        print(f"{p['nombre']} - Vendidos: {p['vendidos']}")
+def report_top_books():
+    print("\n--- TOP 3 libros mas vendidos ---")
+    books_sorted = sorted(inventary, key=lambda p: p["sales"], reverse=True)
+    for v in books_sorted[:3]:
+        print(f"{v['title']} - sales: {v['sales']}")
 
 
-def sales_report_author():
+def report_sales_author():
     print("\n--- Ventas por autor ---")
 
-    author_totals = {}
-    for sale in sales_hitory:
-        product = search_book(sale["producto"])
-        author = product["autor"]
-        author_totals[author] = author_totals.get(author, 0) + sale["neto"]
+    total_authors = {}
+    for sale in sales_history:
+        book = search_book(sale["book"])
+        author = book["author"]
+        total_authors[author] = total_authors.get(author, 0) + sale["neto"]
 
-    for author, total in author_totals.items():
+    for author, total in total_authors.items():
         print(f"{author}: ${total:.2f}")
 
 
-def income_reports():
+def report_income():
     print("\n--- Reporte de ingresos ---")
-    bruto = sum(i["bruto"] for i in sales_hitory)
-    neto = sum(i["neto"] for i in sales_hitory)
+    bruto = sum(s["bruto"] for s in sales_history)
+    neto = sum(s["neto"] for s in sales_history)
     print(f"Ingreso bruto: ${bruto:.2f}")
     print(f"Ingreso neto: ${neto:.2f}")
-
-
-def inventory_performance_report():
-    print("\n--- Rendimiento de inventario ---")
-    for i in inventary:
-        percentage_sold = (i["vendidos"] / (i["vendidos"] + i["stock"]) if i["vendidos"] + i["stock"] > 0 else 0)
-        print(f"{i['nombre']}: {percentage_sold * 100:.2f}% vendidos")
-
-
-
 
 
 main_menu()
